4e8fa7806a19d2f961ff0061b645b4460b31516f
Yun-Seok Lee <ystone0213@naver.com>
2023-04-18 18:02:58 +0900

    230418

---
+++ YStone/Day1.js
@@ -0,0 +1,41 @@
+// 두 수의 합
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+
+const solution = (num1, num2) => num1 + num2;
+
+// 두 수의 차
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50000 ≤ num1 ≤ 50000
+// -50000 ≤ num2 ≤ 50000
+
+const solution = (num1, num2) => num1 - num2;
+
+// 두 수의 곱
+// 문제 설명
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 0 ≤ num1 ≤ 100
+// 0 ≤ num2 ≤ 100
+
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+// 몫 구하기
+// 문제 설명
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 0 < num1 ≤ 100
+// 0 < num2 ≤ 100
+
+const solution = (num1, num2) => Math.floor(num1 / num2);
