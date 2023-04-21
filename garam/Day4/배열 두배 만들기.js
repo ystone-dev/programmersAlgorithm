709e1ff418fe53b68b07955118e247b6160cb0c2
garam.kim <bd64118@naver.com>
2023-04-21 14:42:53 +0900

    Day3 풀이

---
+++ garam/Day4/배열 두배 만들기.js
@@ -0,0 +1,21 @@
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    const result = [];
+    for (let item of numbers) {
+         result.push(item * 2)
+    }
+    return result;
+}
+
+// 다른사람 풀이 map 이용
+
+function solution(numbers) {
+    const results = [];
+
+    numbers.map((item) => {
+        results.push(item*2);
+    })
+
+    return results;
+}
