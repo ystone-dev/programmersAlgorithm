f944deb37ef00b99b7a1de4cf136552ee91cd0f4
Yun-Seok Lee <ystone0213@naver.com>
2023-04-23 18:25:32 +0900

    230423-1

--- YStone/Day6.js
+++ YStone/Day6.js
@@ -14,7 +14,7 @@ const solution = (array, n) => {
 
 const solution = (array, n) => {
   let result = 0;
-  for (let num of array) {
+  for (const num of array) {
     if (num === n) {
       result += 1;
     }
@@ -43,4 +43,19 @@ const solution = (dot) => {
   } else {
     return dot[1] > 0 ? 2 : 3;
   }
+};
+
+// 문자열 뒤집기
+// 문제 설명
+// 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 1,000
+
+const solution = (my_string) => {
+  let result = "";
+  for (const letter of my_string) {
+    result = letter + result;
+  }
+  return result;
 };
