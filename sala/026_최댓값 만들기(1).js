d055256ee6ccd494e087ac48b749ff429af4f56a
sala <113747290+developer-sala@users.noreply.github.com>
2023-04-27 17:52:32 +0900

    230427 문제풀이

---
+++ sala/026_최댓값 만들기(1).js
@@ -0,0 +1,6 @@
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+function solution(numbers) {
+  const num1 = numbers.sort((a, b) => b - a)[0];
+  const num2 = numbers.sort((a, b) => b - a)[1];
+  return num1 * num2;
+}
