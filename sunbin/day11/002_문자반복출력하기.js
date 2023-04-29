75a2e58b760e4be5e2ffaa934e4558de2a7533bc
jsunbin <jsnb6n@gmail.com>
2023-04-29 17:16:29 +0900

    💻 Day11 문자반복출력하기.js 추가

--- sunbin/day11/002_문자반복출력하기.js
+++ sunbin/day11/002_문자반복출력하기.js
@@ -1,3 +1,23 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120825
 
-// 자스는 내일..할래요..머리가 안돌아요ㅠㅠ
+function solution(my_string, n) {
+  const strArray = [...my_string];
+  let newArray = [];
+
+  strArray.forEach((e) => {
+    for (let i = 0; i < n; i++) {
+      newArray.push(e);
+    };
+  })
+
+  const answer = newArray.join("")
+
+  return answer
+}
+
+// 다른풀이
+function solutionB(my_string, n) {
+  const answer = [...my_string].map(v => v.repeat(n)).join("");
+
+  return answer
+}
