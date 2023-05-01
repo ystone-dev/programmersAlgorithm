5855678cfbb22257d0dbbc5caf20c6cd582deb35
sala <113747290+developer-sala@users.noreply.github.com>
2023-05-01 11:52:40 +0900

    230501 문제풀이

--- sala/032_모음 제거.js
+++ sala/032_모음 제거.js
@@ -2,3 +2,14 @@
 function solution(my_string) {
   return my_string.replaceAll(/a|e|i|o|u/g, '');
 }
+
+// 다른 풀이법
+function solution(my_string) {
+  return my_string.replace(/[aeiou]/g, '');
+}
+
+function solution(my_string) {
+  return Array.from(my_string)
+    .filter((t) => !['a', 'e', 'i', 'o', 'u'].includes(t))
+    .join('');
+}
