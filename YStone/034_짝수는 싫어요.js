5877019482430207e562b6fb83eb4db0bf54c27f
Yun-Seok Lee <ystone0213@naver.com>
2023-05-02 21:48:06 +0900

    230502

---
+++ YStone/034_짝수는 싫어요.js
@@ -0,0 +1,16 @@
+// 짝수는 싫어요
+// 문제 설명
+// 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 100
+
+const solution = (n) => {
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 1) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
