dbb007e720a00c805566b7a548f3dffd4862d9c1
Yun-Seok Lee <ystone0213@naver.com>
2023-05-03 10:52:20 +0900

    230503

---
+++ YStone/035_자릿수 더하기.js
@@ -0,0 +1,20 @@
+// 자릿수 더하기
+// 문제 설명
+// 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+
+// 제한사항
+// 0 ≤ n ≤ 1,000,000
+
+const solution = (n) => {
+  return n
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc + parseInt(cur), 0);
+};
+
+const solution = (n) => {
+  const arr = n.toString().split("");
+  let sum = 0;
+  arr.forEach((num) => (sum += parseInt(num)));
+  return sum;
+};
