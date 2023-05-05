820a92c701b8e82a51977c1db8f010dc78c072b2
garam.kim <bd64118@naver.com>
2023-05-05 23:18:08 +0900

    풀이

---
+++ garam/Lv.0/숨어있는 숫자의 덧셈(1).js
@@ -0,0 +1,12 @@
+// 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+
+function solution(my_string) {
+    const num = my_string.split("");
+    let result = 0;
+    
+    for(let i = 0; i < num.length; i++){
+      if(Number(num[i])) result += Number(num[i])
+    }
+    return result
+  }
