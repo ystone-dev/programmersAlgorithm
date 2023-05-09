0ca0ad8016da3841941fc88d4233d6745224266d
garam.kim <bd64118@naver.com>
2023-05-09 21:54:04 +0900

    풀이

---
+++ garam/Lv.0/문자열 정렬하기(1).js
@@ -0,0 +1,5 @@
+// 문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+
+function solution(my_string) {
+    return my_string.replace(/\D/g, '').split('').map(item => Number(item)).sort((a, b) => a - b);
+}
