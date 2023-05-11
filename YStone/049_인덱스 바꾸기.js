18375bc13c6e8a8db90eac9a9795c49ccd80d7cd
Yun-Seok Lee <ystone0213@naver.com>
2023-05-11 17:41:37 +0900

    230511

---
+++ YStone/049_인덱스 바꾸기.js
@@ -0,0 +1,16 @@
+// 인덱스 바꾸기
+// 문제 설명
+// 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 1 < my_string의 길이 < 100
+// 0 ≤ num1, num2 < my_string의 길이
+// my_string은 소문자로 이루어져 있습니다.
+// num1 ≠ num2
+
+const solution = (my_string, num1, num2) => {
+  const arr = my_string.split("");
+  arr[num1] = my_string[num2];
+  arr[num2] = my_string[num1];
+  return arr.join("");
+};
