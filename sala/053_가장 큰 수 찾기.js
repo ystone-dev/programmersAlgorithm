b3483d06d7e947de674cd700bfa5176f1abdef4c
sala <113747290+developer-sala@users.noreply.github.com>
2023-05-14 17:24:37 +0900

    230513 문제풀이(230514에 풀었음)

---
+++ sala/053_가장 큰 수 찾기.js
@@ -0,0 +1,12 @@
+// 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+function solution(array) {
+  const maxNum = [...array].sort((a, b) => b - a)[0];
+  const maxIndex = array.indexOf(maxNum);
+  return [maxNum, maxIndex];
+}
+
+// 다른 풀이법
+function solution(array) {
+  let max = Math.max(...array);
+  return [max, array.indexOf(max)];
+}
