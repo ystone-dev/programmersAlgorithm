7c75143bc2ce42880dfae29dd37c48ec70d60631
sala <113747290+developer-sala@users.noreply.github.com>
2023-05-22 16:17:19 +0900

    Delete 약수의 합.js

--- sala/Lv.1/약수의 합.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-  let answer = [];
-  for (let i = n; i >= 0; i--) {
-    if (n % i === 0) {
-      answer.push(i);
-    }
-  }
-  return answer.reduce((a, b) => a + b, 0);
-}
