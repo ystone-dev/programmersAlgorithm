17652b9835e7035f1e0dd444e15d1405bb83a1be
sala <113747290+developer-sala@users.noreply.github.com>
2023-05-24 16:30:40 +0900

    day31 문제풀이

---
+++ sala/Lv.1/008_자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,14 @@
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+function solution(n) {
+  const newArr = Array.from(String(n), Number);
+  return newArr.reverse();
+}
+
+// 다른 풀이법
+function solution(n) {
+  return n
+    .toString()
+    .split('')
+    .reverse()
+    .map((item) => (item = parseInt(item)));
+}
