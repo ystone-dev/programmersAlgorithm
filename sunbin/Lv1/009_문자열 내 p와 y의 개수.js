8ab117b06dcb0298ff64d01258c80d4383e67451
jsunbin <jsnb6n@gmail.com>
2023-05-30 17:20:32 +0900

    💻 Day32 - 문자열 내 p와 y의 개수 Optional Chaining으로 해결

--- sunbin/Lv1/009_문자열 내 p와 y의 개수.js
+++ sunbin/Lv1/009_문자열 내 p와 y의 개수.js
@@ -1,9 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12916
 
-// 런타임 에러ㅠㅠ
+// 런타임 에러ㅠㅠ -> optional Chaining
 function solution(s) {
-  const p_count = s.match(/p/ig).length;
-  const y_count = s.match(/y/ig).length;
+  const p_count = s.match(/p/ig)?.length;
+  const y_count = s.match(/y/ig)?.length;
 
   return p_count === y_count
 }
