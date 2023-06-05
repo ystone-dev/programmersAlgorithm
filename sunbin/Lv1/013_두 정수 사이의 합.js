ece18478130ee2c1bbb0db889965c84c3cb3f24f
jsunbin <jsnb6n@gmail.com>
2023-06-05 23:51:16 +0900

    💻 Day34 - Lv1

---
+++ sunbin/Lv1/013_두 정수 사이의 합.js
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12912
+
+function solution(a, b) {
+  let answer = 0;
+
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      answer += i
+    }
+  } else {
+      for (let i = b; i <= a; i++) {
+      answer += i
+    }
+  }
+
+  return answer
+}
