425387b3f71dfa3ad2448a17f77b040a08f9a0c5
jsunbin <jsnb6n@gmail.com>
2023-06-07 23:56:27 +0900

    💻 Day35 - Lv1

---
+++ sunbin/Lv1/015_서울에서 김서방 찾기.js
@@ -0,0 +1,6 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+  const answer = '';
+  return answer;
+}
---
+++ sunbin/Lv1/016_나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,6 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12910
+
+function solution(arr, divisor) {
+  const answer = [];
+  return answer;
+}
